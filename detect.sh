746cbdd17fd6c336c2f71963b19d0630493ac603
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-06-02 15:00:53 -0500

    detect 10 10

--- detect.sh
+++ detect.sh
@@ -5,5 +5,5 @@ export PYTHONPATH=".":$PYTHONPATH
 method=$1
 Tindex=$2
 
-python detect.py --token_file "results/opt-${method}-20-20.p" --n 20 --model facebook/opt-1.3b --seed 1 --Tindex ${Tindex} --k 20 --method ${method} --n_runs 499
-python detect.py --token_file "results/gpt-${method}-20-20.p" --n 20 --model openai-community/gpt2 --seed 1 --Tindex ${Tindex} --k 20 --method ${method} --n_runs 499
+python detect.py --token_file "results/opt-${method}-10-10.p" --n 10 --model facebook/opt-1.3b --seed 1 --Tindex ${Tindex} --k 10 --method ${method} --n_runs 999
+python detect.py --token_file "results/gpt-${method}-10-10.p" --n 10 --model openai-community/gpt2 --seed 1 --Tindex ${Tindex} --k 10 --method ${method} --n_runs 999
