30d92404569814903d2c58a89146619f59f81d2d
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-06-03 23:28:40 -0500

    Add analyization

--- README.md
+++ README.md
@@ -1,1 +1,139 @@
-# llm-watermark-adaptive
+# llm-watermark-cpd
+
+## Prerequisites
+
+<details closed>
+<summary>Python environments</summary>
+
+-   Cython==3.0.10
+-   datasets==2.19.1
+-   huggingface_hub==0.23.0
+-   nltk==3.8.1
+-   numpy==1.26.4
+-   sacremoses==0.0.53
+-   scipy==1.13.0
+-   sentencepiece==0.2.0
+-   tokenizers==0.19.1
+-   torch==2.3.0.post100
+-   torchaudio==2.3.0
+-   torchvision==0.18.0
+-   tqdm==4.66.4
+-   transformers==4.40.2
+
+</details>
+
+### Set up environments
+
+#### Python
+
+> [!NOTE]
+> Refer to https://pytorch.org for PyTorch installation on other platforms
+
+```shell
+# conda install pytorch torchvision torchaudio cpuonly -c pytorch
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install conda-forge::transformers
+conda install cython scipy nltk sentencepiece sacremoses
+```
+
+#### R
+
+> [!NOTE]
+> R is used for change point detection. Refer to https://www.r-project.org for
+> installation instructions.
+
+```r
+install.packages(c("doParallel", "reshape2", "ggplot2", "fossil"))
+```
+
+## Instruction
+
+To reproduce the results, follow the instructions below or use the attached
+results directly using `Rscript analyze.R 1 3200`.
+
+### Set up pyx
+
+```shell
+python setup.py build_ext --inplace
+```
+
+#### Expected running time
+
+Less than 1 minute on a single core CPU machine.
+
+#### Expected memory usage
+
+Less than 1 GB.
+
+### Generate watermarked tokens
+
+```shell
+mkdir -p results
+
+export PYTHONPATH=".":$PYTHONPATH
+
+for method in gumbel; do
+  python textgen.py --save results/opt-$method-10-10.p --n 10 --batch_size 25 --m 10 --model facebook/opt-1.3b --seed 1 --T 1000 --method $method
+done
+
+for method in gumbel; do
+  python textgen.py --save results/gpt-$method-10-10.p --n 10 --batch_size 25 --m 10 --model openai-community/gpt2 --seed 1 --T 1000 --method $method
+done
+```
+
+#### Expected running time
+
+Less than 10 minutes on 1 compute node with 1 NVIDIA A30 GPU and 128 CPU cores.
+
+#### Expected memory usage
+
+Less than 128 GB.
+
+### Calculate p-values for texts
+
+```shell
+mkdir -p log
+
+for method in gumbel; do
+  mkdir -p results/opt-$method-10-10.p-detect
+done
+
+for method in gumbel; do
+  mkdir -p results/gpt-$method-10-10.p-detect
+done
+
+chmod +x ./detect.sh
+parallel -j 10 --progress ./detect.sh {1} {2} ::: gumbel ::: $(seq 1 10)
+```
+
+#### Expected running time
+
+Less than 24 hours on 8 compute nodes with no GPU and 28 CPU cores each.
+
+#### Expected memory usage
+
+Less than 10 GB per compute node.
+
+### Change point analysis
+
+```shell
+parallel -j 8 --progress Rscript analyze.R {1} {2} ::: $(seq 1 400 2801) ::: $(seq 400 400 3200)
+Rscript analyze.R 1 3200
+```
+
+#### Expected running time
+
+Less than 12 hours on 8 compute nodes with no GPU and 28 CPU cores each.
+
+##### Running time test
+
+The following command should run in less than 10 minutes on 1 compute node
+with no GPU and 28 CPU cores.
+
+```shell
+Rscript analyze.R 1 5
+```
+
+#### Expected memory usage
+
+Less than 10 GB per compute node.
---
+++ analyze.R
@@ -0,0 +1,126 @@
+set.seed(1)
+
+folder <- "results/"
+models <- c("facebook/opt-1.3b", "openai-community/gpt2")
+models_folders_prefix <- c("opt", "gpt")
+generation_methods <- c("gumbel")
+n <- 10
+m <- 10
+watermarked_or_null <- c("watermarked")
+
+pvalue_files_templates <- matrix(NA, 0, 11)
+for (model_index in seq_along(models)) {
+  for (generation_methods_index in seq_along(generation_methods)) {
+    for (watermarked_or_null_index in seq_along(watermarked_or_null)) {
+      pvalue_files_templates <- rbind(pvalue_files_templates, c(
+        folder,
+        models_folders_prefix[model_index],
+        "-",
+        generation_methods,
+        "-",
+        n,
+        "-",
+        m,
+        ".p-detect/",
+        watermarked_or_null[watermarked_or_null_index],
+        "-XXX.csv"
+      ))
+    }
+  }
+}
+# filenames_to_settings <- list(
+#   "Setting 1, Gumbel",
+#   "Setting 1, Gumbel Edit",
+#   "Setting 1, Inverse Transform",
+#   "Setting 1, Inverse Transform Edit",
+#   "Setting 2, Gumbel",
+#   "Setting 2, Gumbel Edit",
+#   "Setting 2, Inverse Transform",
+#   "Setting 2, Inverse Transform Edit",
+#   "Setting 3, Gumbel",
+#   "Setting 3, Gumbel Edit",
+#   "Setting 3, Inverse Transform",
+#   "Setting 3, Inverse Transform Edit",
+#   "Setting 4, Gumbel",
+#   "Setting 4, Gumbel Edit",
+#   "Setting 4, Inverse Transform",
+#   "Setting 4, Inverse Transform Edit"
+# )
+# filenames_to_settings <- rep(filenames_to_settings, length(models))
+# names(filenames_to_settings) <- pvalue_files_templates
+
+prompt_count <- 250
+df <- matrix(NA, 0, 6)
+
+for (template_index in seq_len(nrow(pvalue_files_templates))) {
+  for (prompt_index in seq_len(prompt_count)) {
+    # Python index in the file name
+    # filename <- sub("XXX", prompt_index - 1, pvalue_files_template)
+    filename <- sub("XXX", prompt_index, paste0(pvalue_files_templates[template_index, ], collapse = ""))
+    df <- rbind(
+      df,
+      c(
+        pvalue_files_templates[template_index, 2],
+        pvalue_files_templates[template_index, 4],
+        prompt_index,
+        read.csv(filename, header = FALSE)
+      )
+    )
+  }
+}
+
+df <- data.frame(df)
+names(df) <- c(
+  "LLM", "GenerationMethod", "PromptIndex", "Metric 1", "Metric 2", "Metric 3"
+)
+df <- as.data.frame(lapply(df, unlist))
+
+for (model_prefix in models_folders_prefix) {
+  hist_df <- rbind(
+    cbind("Metric 1", df[df$LLM == model_prefix, "Metric.1"]),
+    cbind("Metric 2", df[df$LLM == model_prefix, "Metric.2"]),
+    cbind("Metric 3", df[df$LLM == model_prefix, "Metric.3"])
+  )
+  hist_df <- data.frame(hist_df)
+  names(hist_df) <- c("Metric", "Value")
+  hist_df <- as.data.frame(lapply(hist_df, unlist))
+  hist_df$Metric <-
+    factor(hist_df$Metric, levels = c("Metric 1", "Metric 3", "Metric 2"))
+  hist_df$Value <- as.numeric(hist_df$Value)
+
+  print(aggregate(hist_df$Value <= 0.05, by = list(hist_df$Metric), FUN = mean))
+  print(aggregate(hist_df$Value <= 0.01, by = list(hist_df$Metric), FUN = mean))
+
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_histogram(
+      ggplot2::aes(x = Value, fill = Metric),
+      data = hist_df,
+      bins = 20
+    ) +
+    ggplot2::facet_wrap(~Metric) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none")
+  ggplot2::ggsave(paste0("results/", model_prefix, "-histogram.pdf"), p, width = 7, height = 7)
+
+  comparison_bipartite <- rbind(
+    cbind("Metric 1", df[df$LLM == model_prefix, "Metric.1"], "Metric 3", df[df$LLM == model_prefix, "Metric.3"], df[df$LLM == model_prefix, "Metric.1"] >= df[df$LLM == model_prefix, "Metric.3"]),
+    cbind("Metric 3", df[df$LLM == model_prefix, "Metric.3"], "Metric 2", df[df$LLM == model_prefix, "Metric.2"], df[df$LLM == model_prefix, "Metric.3"] >= df[df$LLM == model_prefix, "Metric.2"])
+  )
+  comparison_bipartite <- data.frame(comparison_bipartite)
+  names(comparison_bipartite) <- c("x", "y", "xend", "yend", "better")
+  comparison_bipartite <- as.data.frame(lapply(comparison_bipartite, unlist))
+  comparison_bipartite$y <- as.numeric(comparison_bipartite$y)
+  comparison_bipartite$yend <- as.numeric(comparison_bipartite$yend)
+
+  p <- ggplot2::ggplot(comparison_bipartite, ggplot2::aes(x = x, y = y, xend = xend, yend = yend, color = better)) +
+    ggplot2::geom_segment() +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::scale_y_log10() +
+    ggplot2::scale_x_discrete(
+      limits = c("Metric 1", "Metric 3", "Metric 2"),
+      labels = c("Metric 1", "Metric 3", "Metric 2"),
+      expand = c(0, 0.1)
+    )
+  ggplot2::ggsave(paste0("results/", model_prefix, "-comparison_bipartite.pdf"), p, width = 7, height = 7)
+}
