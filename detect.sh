58f669da2744104811f71ce29322f37591b65dc8
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-06-07 15:42:39 -0500

    more partition

--- detect.sh
+++ detect.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=medium
+#SBATCH --partition=medium,long,xlong
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --output=/home/anthony.li/out/detect.%A.%a.out
 #SBATCH --error=/home/anthony.li/out/detect.%A.%a.err
