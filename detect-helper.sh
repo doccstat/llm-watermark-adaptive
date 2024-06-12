26ceeaae6222642658aa066a43e5c78c7e20bba6
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-06-12 00:45:18 -0500

    20-20-10

--- detect-helper.sh
+++ detect-helper.sh
@@ -7,5 +7,9 @@ Tindex=$2
 attack=$3
 pcts=$4
 
-python detect.py --token_file "results/opt-${method}-${attack}-30-30-${pcts}.p" --n 30 --model facebook/opt-1.3b --seed 1 --Tindex ${Tindex} --k 30 --method ${method} --n_runs 999
-python detect.py --token_file "results/gpt-${method}-${attack}-30-30-${pcts}.p" --n 30 --model openai-community/gpt2 --seed 1 --Tindex ${Tindex} --k 30 --method ${method} --n_runs 999
+n=20
+m=20
+k=10
+
+python detect.py --token_file "results/opt-${method}-${attack}-${n}-${m}-${pcts}.p" --n ${n} --model facebook/opt-1.3b --seed 1 --Tindex ${Tindex} --k ${k} --method ${method} --n_runs 999
+python detect.py --token_file "results/gpt-${method}-${attack}-${n}-${m}-${pcts}.p" --n ${n} --model openai-community/gpt2 --seed 1 --Tindex ${Tindex} --k ${k} --method ${method} --n_runs 999
