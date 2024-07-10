37d4b4e8754a0a0bbc9a48eff30faeeb86c242ba
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-07-10 15:52:21 -0500

    download dataset

--- download.py
+++ download.py
@@ -1,7 +1,13 @@
+from os.path import exists
+from os import makedirs
 from datasets import load_dataset
 
 # Load the dataset (this might take a while depending on your internet connection and the dataset size)
 dataset = load_dataset("allenai/c4", "realnewslike", split="train")
 
 # Save the dataset locally
-dataset.save_to_disk('/scratch/user/anthony.li/datasets/c4_realnewslike_train/')
+if not exists("/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train"):
+    makedirs("/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train")
+    dataset.save_to_disk(
+        '/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train'
+    )
