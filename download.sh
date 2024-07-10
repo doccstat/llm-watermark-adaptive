ddd73667cc2cda35f88b3e8a373a0e8ba8a54009
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-07-10 16:00:12 -0500

    a day to download

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=download
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-00:05:00
+#SBATCH --time=1-00:00:00
 #SBATCH --partition=medium,long,xlong
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --output=/home/anthony.li/out/download.%j.out
