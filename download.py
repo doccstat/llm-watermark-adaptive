278e46e953d8e5b7373f02026a0f11ce9f4201e2
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-07-10 16:10:48 -0500

    download model

--- download.py
+++ download.py
@@ -1,13 +1,25 @@
 from os.path import exists
 from os import makedirs
 from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # Load the dataset (this might take a while depending on your internet connection and the dataset size)
 dataset = load_dataset("allenai/c4", "realnewslike", split="train")
+dataset_path = "/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train"
 
 # Save the dataset locally
-if not exists("/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train"):
-    makedirs("/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train")
-    dataset.save_to_disk(
-        '/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train'
-    )
+if not exists(dataset_path):
+    makedirs(dataset_path)
+    dataset.save_to_disk(dataset_path)
+
+model_path = "/scratch/user/anthony.li/models/"
+
+for model_name in [
+    "meta-llama/Meta-Llama-3-8B", "openai-community/gpt2", "facebook/opt-1.3b"
+]:
+    if not exists(model_path + model_name):
+        makedirs(model_path + model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenizer.save_pretrained(model_path + model_name + "/tokenizer")
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+        model.save_pretrained(model_path + model_name + "/model")
