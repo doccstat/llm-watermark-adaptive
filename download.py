a0187f433396059dde44da456e7996ed15d455f8
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-08-05 10:10:39 -0700

    mistralai/Mistral-Nemo-Instruct-2407

--- download.py
+++ download.py
@@ -15,7 +15,7 @@ if not exists(dataset_path):
 model_path = "/scratch/user/anthony.li/models/"
 
 for model_name in [
-    "meta-llama/Meta-Llama-3-8B", "openai-community/gpt2", "facebook/opt-1.3b", "mistralai/Mistral-Large-Instruct-2407"
+    "meta-llama/Meta-Llama-3-8B", "openai-community/gpt2", "facebook/opt-1.3b", "mistralai/Mistral-Nemo-Instruct-2407"
 ]:
     if not exists(model_path + model_name):
         makedirs(model_path + model_name)
