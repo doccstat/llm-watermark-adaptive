ab1b62d98791bac50ce84f6f5f3ed0534fa45483
Anthony Li <38049831+doccstat@users.noreply.github.com>
2024-08-21 22:15:49 -0500

    mistralai/Mistral-7B-v0.1

--- 2-download.py
+++ 2-download.py
@@ -3,7 +3,8 @@ from os import makedirs
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Load the dataset (this might take a while depending on your internet connection and the dataset size)
+# Load the dataset (this might take a while depending on the internet
+# connection and the dataset size)
 dataset = load_dataset("allenai/c4", "realnewslike", split="train")
 dataset_path = "/scratch/user/anthony.li/datasets/allenai/c4/realnewslike/train"
 
@@ -15,7 +16,10 @@ if not exists(dataset_path):
 model_path = "/scratch/user/anthony.li/models/"
 
 for model_name in [
-    "meta-llama/Meta-Llama-3-8B", "openai-community/gpt2", "facebook/opt-1.3b", "mistralai/Mistral-Nemo-Instruct-2407"
+    "meta-llama/Meta-Llama-3-8B",
+    "openai-community/gpt2",
+    "facebook/opt-1.3b",
+    "mistralai/Mistral-7B-v0.1"
 ]:
     if not exists(model_path + model_name):
         makedirs(model_path + model_name)
--- 3-textgen.sh
+++ 3-textgen.sh
@@ -2,15 +2,15 @@
 
 #SBATCH --job-name=textgen
 #SBATCH --nodes=1
-#SBATCH --ntasks=128
-#SBATCH --ntasks-per-node=128
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=1
 
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=gpu,xgpu
 #SBATCH --gres=gpu:a30:2
 
-#SBATCH --mem=128GB
+#SBATCH --mem=64GB
 #SBATCH --output=/home/anthony.li/out/textgen.%A.%a.out
 #SBATCH --error=/home/anthony.li/out/textgen.%A.%a.err
 #SBATCH --mail-type=ALL
@@ -48,9 +48,9 @@ for watermark_key_length in 20 50 80 100 500 1000; do
 
   for method in gumbel; do
     for pcts in 0.0 0.05 0.1 0.2 0.3; do
-      python 3-textgen.py --save results/gpt-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model openai-community/gpt2 --seed 1 --T 1000 --method $method --${attack} $pcts --candidate_prompt_max 10
-      python 3-textgen.py --save results/opt-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model facebook/opt-1.3b --seed 1 --T 1000 --method $method --${attack} $pcts --candidate_prompt_max 10
-      python 3-textgen.py --save results/ml3-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 1000 --method $method --${attack} $pcts --candidate_prompt_max 10
+      python 3-textgen.py --save results/gpt-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model openai-community/gpt2 --seed 1 --T 100 --method $method --${attack} $pcts --candidate_prompt_max 10
+      # python 3-textgen.py --save results/opt-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model facebook/opt-1.3b --seed 1 --T 100 --method $method --${attack} $pcts --candidate_prompt_max 10
+      python 3-textgen.py --save results/ml3-$method-$attack-$watermark_key_length-$tokens_count-$pcts.p --watermark_key_length $watermark_key_length --batch_size 100 --tokens_count $tokens_count --buffer_tokens 0 --model meta-llama/Meta-Llama-3-8B --seed 1 --T 100 --method $method --${attack} $pcts --candidate_prompt_max 10
     done
   done
 done
